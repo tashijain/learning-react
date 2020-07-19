d4f21919a299b15db8ceb3806ddb9994a6a50e6c
Tashi Jain <tashjain88@gmail.com>
2020-07-19 16:23:16 -0700

    components part1

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import MenuComponent from './components/MenuComponent'
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <MenuComponent />
     </div>
   );
 }
