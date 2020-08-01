15598aa4910c20cc3c3f5a97927ee0e5da377e90
Tashi Jain <tashjain88@gmail.com>
2020-07-31 22:59:04 -0700

    Updates

---
+++ precache-manifest.299e159718576a8e95a3471aecd8f6c9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650dc49aa7cae1f164d99b85aa23f393",
+    "url": "/restaurant-web-app-react/index.html"
+  },
+  {
+    "revision": "378811ab3e271d195b98",
+    "url": "/restaurant-web-app-react/static/css/2.4b180125.chunk.css"
+  },
+  {
+    "revision": "07951353247ce04ac0d3",
+    "url": "/restaurant-web-app-react/static/css/main.d1679a28.chunk.css"
+  },
+  {
+    "revision": "378811ab3e271d195b98",
+    "url": "/restaurant-web-app-react/static/js/2.40cc3072.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/restaurant-web-app-react/static/js/2.40cc3072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07951353247ce04ac0d3",
+    "url": "/restaurant-web-app-react/static/js/main.f02b93f1.chunk.js"
+  },
+  {
+    "revision": "977de8f2b96b8129c2c1",
+    "url": "/restaurant-web-app-react/static/js/runtime-main.79df147f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/restaurant-web-app-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/restaurant-web-app-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/restaurant-web-app-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/restaurant-web-app-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/restaurant-web-app-react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
